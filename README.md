95a11f9110bd736550c89035a11a07304e2be4e6
Sefinek <contact@sefinek.net>
2024-09-03 17:56:22 +0200

    Main commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Node Page Capturer
 Captures a screenshot of any webpage. And that's all...
+
+## CAPTURE_FULL_PAGE = false
+![page_2024-09-03T15-48-36-628Z.png](images/page_2024-09-03T15-48-36-628Z.png)
+![page_2024-09-03T15-35-54-125Z.png](images/page_2024-09-03T15-35-54-125Z.png)
+
+## CAPTURE_FULL_PAGE = true
+![fullscreen_2024-09-03T15-46-57-880Z.png](images/fullscreen_2024-09-03T15-46-57-880Z.png)
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 
-const DOMAIN = 'https://www.youtube.com/watch?v=BnnbP7pCIvQ';
+const DOMAIN = 'https://duckduckgo.com/?q=cat&iax=images&ia=images';
 const CAPTURE_FULL_PAGE = false;
 
 const sanitizeFileName = url => url.replace(/[:\\/?&=]/g, '_');
@@ -12,7 +12,8 @@ const formatTimestamp = () => new Date().toISOString().replace(/[:.]/g, '-');
 (async () => {
 	try {
 		const browser = await puppeteer.launch({
-			headless: true,
+			headless: false,
+			defaultViewport: null,
 			args: [
 				'--no-sandbox',
 				'--disable-setuid-sandbox',
@@ -39,20 +40,26 @@ const formatTimestamp = () => new Date().toISOString().replace(/[:.]/g, '-');
 				'--no-pings',
 				'--password-store=basic',
 				'--use-mock-keychain',
-				'--disable-webrtc'
+				'--disable-webrtc',
+				'--start-maximized'
 			]
 		});
 
 		const page = await browser.newPage();
+
+		// Setting User-Agent etc.
+		await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36');
 		await page.setViewport({ width: 1920, height: 1080 });
+
+		// Navigate to the target page
 		await page.goto(DOMAIN, { waitUntil: 'networkidle2', timeout: 10000 });
 
+		// Waiting for the full content to load
 		await new Promise(resolve => setTimeout(resolve, 1000));
 
-		const sanitizedDomain = sanitizeFileName(new URL(DOMAIN).hostname);
-		const saveDir = path.join(__dirname, 'screenshots', sanitizedDomain, formatDate());
+		// Taking a screenshot
+		const saveDir = path.join(__dirname, 'screenshots', sanitizeFileName(new URL(DOMAIN).hostname), formatDate());
 		fs.mkdirSync(saveDir, { recursive: true });
-
 		await page.screenshot({
 			path: path.join(saveDir, `${CAPTURE_FULL_PAGE ? 'fullscreen' : 'page'}_${formatTimestamp()}.png`),
 			fullPage: CAPTURE_FULL_PAGE
